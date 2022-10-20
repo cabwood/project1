8ed507efcbad687fb60e0520247cc64cc2bd9e30
Simon Fitch <firstpotato@gmail.com>
2022-10-20 13:52:32 +0400

    Added an example of lists and dicts in action

---
+++ parse_license.py
@@ -0,0 +1,38 @@
+find_words = [
+    "copyright",
+    "the",
+    "a",
+]
+
+word_counts = {}
+
+for word in find_words:
+    word_counts[word] = 0
+
+copyright_count = 0
+
+def parse(ln):
+    global copyright_count
+    words = ln.split()
+    for word in words:
+        try:
+            num = int(word)
+        except:
+            pass
+        else:
+            print(" There was a number: ", num)
+        lword = word.lower()
+        for search_word in find_words:
+            if search_word in lword:
+                word_counts[search_word] += 1
+
+f = open("LICENSE", "r")
+try:
+    lines = f.readlines()
+    for line in lines:
+        parse(line)
+finally:
+    f.close()
+
+for word, count in word_counts.items():
+    print(f'There were {count} occurrences of the word {word}')
